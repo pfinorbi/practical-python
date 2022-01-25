d5d45919ad64ffe828ac9805d6f083bcb11f683d
pfinorbi <84071132+pfinorbi@users.noreply.github.com>
2022-01-25 23:44:29 +0100

    completed 1.2 A First Program

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100 # meters
+num_of_bounces = 1
+
+while num_of_bounces <= 10:
+    num_of_bounces = num_of_bounces + 1
+    height = ( height / 5 ) * 3
+    print(round(height, 4))
