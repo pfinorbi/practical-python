91825cbb562ce8ea8a51e8717751b8d16212aaf6
pfinorbi <84071132+pfinorbi@users.noreply.github.com>
2022-02-21 23:49:24 +0100

    1.6 File Management completed

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,16 @@
 # pcost.py
 #
 # Exercise 1.27
+count = 0
+total_cost = 0.0
+
+with open('Data/portfolio.csv', 'rt') as portfolio:
+    for row in portfolio:
+        if(count > 0):
+           shares_and_prices = row.split(',')
+           current_share = int(shares_and_prices[1])
+           current_price = float(shares_and_prices[2])
+           total_cost = total_cost + (current_share * current_price)
+        count = count + 1
+
+print(f'Total cost: {total_cost}')
