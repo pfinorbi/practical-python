65e9fa350c4fede994b0a909e391237f85ceefa9
pfinorbi <84071132+pfinorbi@users.noreply.github.com>
2023-05-09 00:22:47 +0200

    2.3 Formatting completed

--- Work/report.py
+++ Work/report.py
@@ -34,11 +34,18 @@ def read_prices(filename):
         pass
   return prices
 
+def make_report(portfolio, prices):
+
+  stock_changes = []
+
+  for item in portfolio:
+    stock_changes.append((item['name'], item['shares'], prices[item['name']], prices[item['name']] - item['price']))
+  
+  return stock_changes
+
 portfolio = read_portfolio('Data/portfolio.csv')
-#print(portfolio)
 
 prices = read_prices('Data/prices.csv')
-#print(prices)
 
 purchase_value = 0.0
 current_value = 0.0
@@ -50,6 +57,26 @@ for element in portfolio:
   
 profit = current_value - purchase_value
 
+report = make_report(portfolio, prices)
+
+headers = ('Name', 'Shares', 'Price', 'Change')
+formatted_headers = f'{headers[0]:>10s} {headers[1]:>10s} {headers[2]:>10s} {headers[3]:>10s}'
+
+separator = []
+
+for h in headers:
+  separator.append(f'{h.replace(h,"-"):->10s}')
+
+print("\nSummary:\n")
 print(f"Purchase value: {purchase_value}")
 print(f"Current value: {current_value}")
 print(f"Profit: {profit:.2f}")
+
+print("\nDetailed price change report:\n")
+print(formatted_headers)
+print(f'{separator[0]} {separator[1]} {separator[2]} {separator[3]}')
+
+for name, shares, price, change in report:
+  updated_price = str(price).replace(str(price), "$"+str(price))
+  print(f'{name:>10s} {shares:>10d} {updated_price:>10s} {change:>10.2f}')
+
